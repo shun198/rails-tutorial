7dd52789f2c526e7b0ac2fef59c27bd122de2db2
shun198 <shunhiroseluvmri@gmail.com>
2022-05-31 07:14:49 +0900

    fix:enable drop down menu to appear
    ドロップダウンメニューが表示されないバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash.now[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
+                                  :password_confirmation)
     end
 end
